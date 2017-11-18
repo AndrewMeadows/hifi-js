4412b3b326f6cfa49d90e7b1d8418e9c5b8200c9
Andrew Meadows <andrew@highfidelity.io>
2017-11-17 16:41:18 -0800

    add random ID to computePi output

--- torture/computePi.js
+++ torture/computePi.js
@@ -1,6 +1,7 @@
 // computePi.js -- torture: needlessly crunch numbers in entity-script
 (function() {
     this.preload = function() {
+        var randomNumber = Math.floor(Math.random() * 100000);
         this.timerID = Script.setInterval(function() {
         	// This infinite series computes PI but converges slowly.
         	// π = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) ...
@@ -11,7 +12,7 @@
             	pi += sign * 4.0 / (2.0 * i + 1.0);
             	sign *= -1.0;
         	}
-        	print("π = " + pi);
+        	print(randomNumber + "  π = " + pi);
         }, 1);
     };
     this.unload = function() {
