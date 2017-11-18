bc59e71b38c96505706a0bbb54c70290026d21e6
Andrew Meadows <andrew@highfidelity.io>
2017-11-17 16:08:14 -0800

    computePi.js is now a working entity-script

--- torture/computePi.js
+++ torture/computePi.js
@@ -1,21 +1,20 @@
-// computePi.js -- torture: needlessly crunch numbers on update
-
+// computePi.js -- torture: needlessly crunch numbers in entity-script
 (function() {
-    print("adebug computePi.js");
-    var computeCount = 0;
-    function computePi(computeTime) {
-        // This infinite series computes PI, but it converges slowly.
-        // π = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) ...
-        var numIterations = 1e8; // 1e8 iterations should take 2-4 seconds
-        var pi = 0;
-        var sign = 1.0;
-        for (i = 0; i < numIterations; i++) {
-            pi += sign * 4.0 / (2.0 * i + 1.0);
-            sign *= -1.0;
-        }
-        computeCount++;
-        print("computeCount = " + computeCount + "  computeTime = " + dt + "  pi = " + pi);
-    }
-
-    Script.update.connect(computePi);
-})
+    this.preload = function() {
+        this.timerID = Script.setInterval(function() {
+        	// This infinite series computes PI but converges slowly.
+        	// π = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) ...
+        	var numIterations = 1e8; // 1e8 iterations should take 2-4 seconds
+        	var pi = 0;
+        	var sign = 1.0;
+        	for (var i = 0; i < numIterations; i++) {
+            	pi += sign * 4.0 / (2.0 * i + 1.0);
+            	sign *= -1.0;
+        	}
+        	print("π = " + pi);
+        }, 1);
+    };
+    this.unload = function() {
+        Script.clearInterval(this.timerID);
+    };
+});
