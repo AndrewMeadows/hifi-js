5875c41e9415a3f1753ad793d996a5db58e09b8e
Andrew Meadows <andrew@highfidelity.io>
2018-01-17 09:10:03 -0800

    fix!

--- example/pushBall.js
+++ example/pushBall.js
@@ -1,11 +1,12 @@
 // pushBall.js
 //
 // interactive ball for multi-user play
-// whoever clicks on ball will set velocity to <0,1,0>
 //
+
 (function () {
     this.entityID = null;
     var GRAVITY = {x: 0, y: -1.5, z: 0};
+
     this.preload = function (entityID) {
         this.entityID = entityID;
         Entities.editEntity(this.entityID, {
@@ -17,26 +18,28 @@
             userData: "{\"grabbableKey\": { \"wantsTrigger\": true, \"grabbable\": false  }}"
         })
     }
+
     this.push = function() {
         var newVelocity = { x: 0, y: 1, z: 0 };
         Entities.editEntity(this.entityID, {velocity: newVelocity});
     }
+
     this.startFarTrigger = function (entityID) {
         if (entityID === this.entityID) {
-            push();
+            this.push();
         }
     };
     this.startNearGrab = function (entityID) {
         if (entityID === this.entityID) {
-            push();
+            this.push();
         }
     };
+
     this.clickDownOnEntity = function (entityID, mouseEvent) {
         if (entityID === this.entityID) {
             if (mouseEvent.isLeftButton) {
-                push();
+                this.push();
             }
         }
     };
 });
-
