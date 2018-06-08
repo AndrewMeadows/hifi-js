c8af5fa88b8e7f8c7af818eebb8889ab9f197004
Andrew Meadows <andrew@highfidelity.io>
2018-06-07 17:43:43 -0700

    nBody: update gravity instead of velocity

--- example/nBody.js
+++ example/nBody.js
@@ -2,13 +2,12 @@
 //  nBody2.js
 //  examples
 //
-//  Created by Andrew Meadows, simple N-body gravitational interaction, 2018.05.05
+//  Created by Andrew Meadows, simple N-body 1/r interaction, 2018.05.05
 //  Copyright 2018 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-
 // BEGIN Vec3 utilities
 var VEC3_UTIL_EPSILON=0.000001;
 var VEC3_UTIL_EPSILON_SQUARED=VEC3_UTIL_EPSILON * VEC3_UTIL_EPSILON;
@@ -37,11 +36,6 @@ function angleBetweenVec3s(A,B) {
     return Math.acos(cosAngle);
 } // END Vec3 utilities
 
-var NUM_PLANETS = 4;
-var SYSTEM_RADIUS = 10;
-var PLANET_DIAMETER = 0.5;
-var LIFETIME = 300;
-
 var RED = { red: 255, green: 0, blue: 0 };
 var GREEN = { red: 0, green: 255, blue: 0 };
 var BLUE = { red: 0, green: 0, blue: 255 };
@@ -53,109 +47,132 @@ var COLORS = [ RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA ];
 var NUM_COLORS = COLORS.length;
 var ZERO_VEC3 = { x: 0, y: 0, z: 0 };
 
+
+var NUM_PLANETS = 4;
+var SYSTEM_RADIUS = 8;
+var PLANET_DIAMETER = 0.5;
+var PLANET_DENSITY = 1000.0;
+var PLANET_MASS = PLANET_DENSITY * Math.PI * PLANET_DIAMETER * PLANET_DIAMETER * PLANET_DIAMETER / 6.0;
+var SUN_DIAMETER = 2.0 * PLANET_DIAMETER;
+var SUN_DENSITY = 3 * PLANET_DENSITY;
+var SUN_MASS = SUN_DENSITY * Math.PI * SUN_DIAMETER * SUN_DIAMETER * SUN_DIAMETER / 6.0;
+var LIFETIME = 300;
+var FIRST_ORBIT_OFFSET = SUN_DIAMETER + SYSTEM_RADIUS / 6.0;
 var forward = Quat.getFront(MyAvatar.orientation);
-
-// sun is an anchor and doesn't move
-var sunPosition = addVec3s(MyAvatar.position, scaleVec3(SYSTEM_RADIUS, forward));
-var planetMass = 1.0;
-var sunDimensions = { x: PLANET_DIAMETER, y: PLANET_DIAMETER, z: PLANET_DIAMETER };
-var sunMass = 20.0 * planetMass;
-var sun = Entities.addEntity({
-    type: "Sphere",
-    name: "sun",
-    color: WHITE,
-    dimensions: sunDimensions,
-    position: sunPosition,
-    velocity: ZERO_VEC3,
-    damping: 0.0,
-    angularDamping: 0.0,
-    dynamic: false,
-    restitution: 1.0,
-    lifetime: LIFETIME
-});
-
-// planets move around sun
-var planets = [];
-var deltaPhase = 2.0 * Math.PI / NUM_PLANETS;
-var firstOrbitOffset = SYSTEM_RADIUS / 4.0;
-var deltaOrbit = (SYSTEM_RADIUS - firstOrbitOffset) / NUM_PLANETS;
-var planetDimensions = { x: PLANET_DIAMETER, y: PLANET_DIAMETER, z: PLANET_DIAMETER };
+var SUN_POSITION = addVec3s(MyAvatar.position, scaleVec3(2 * SUN_DIAMETER, forward));
+var PLANET_MASS = 1.0;
+var SUN_MASS = 20.0 * PLANET_MASS;
 
 // we compute a G that will provide reasonable stable velocities at the lowest initial orbit
 // v = sqrt(G * totalMass / orbitRadius)
 // G = (orbitRadius * v^2) / totalMass
 var maxOrbitSpeed = 1.0;
-var G = firstOrbitOffset * maxOrbitSpeed * maxOrbitSpeed / (planetMass + sunMass);
+var G = FIRST_ORBIT_OFFSET * maxOrbitSpeed * maxOrbitSpeed / (PLANET_MASS + SUN_MASS);
 
-for (var i = 0; i < NUM_PLANETS; i++) {
-    var colorIndex = i % NUM_COLORS;
-
-    var orbitRadius = firstOrbitOffset + i * deltaOrbit;
-    var phase = - i * deltaPhase;
-    var c = Math.cos(phase);
-    var s = Math.sin(phase);
-    var position = addVec3s(sunPosition, scaleVec3(orbitRadius, { x: c, y: 0, z: s }));
-    var totalMass = planetMass + sunMass;
-    var speed = Math.sqrt(G * totalMass / orbitRadius);
-    var velocity = scaleVec3(speed, { x: s, y: 0, z: -c });
-
-    planets.push(Entities.addEntity({
+var bodies = [];
+function createSun() {
+    var sunDimensions = { x: SUN_DIAMETER, y: SUN_DIAMETER, z: SUN_DIAMETER };
+    bodies.push(Entities.addEntity({
         type: "Sphere",
-        name: "planet",
-        color: COLORS[colorIndex],
-        dimensions: planetDimensions,
-        position: position,
-        velocity: velocity,
-        gravity: ZERO_VEC3,
+        name: "sun",
+        color: WHITE,
+        dimensions: sunDimensions,
+        position: SUN_POSITION,
+        velocity: ZERO_VEC3,
         damping: 0.0,
         angularDamping: 0.0,
-        dynamic: true,
+        dynamic: false,
         restitution: 1.0,
+        density: SUN_DENSITY,
         lifetime: LIFETIME
     }));
 }
+createSun();
+
+function createPlanets() {
+    var deltaPhase = 2.0 * Math.PI / NUM_PLANETS;
+    var deltaOrbit = (SYSTEM_RADIUS - FIRST_ORBIT_OFFSET) / NUM_PLANETS;
+    var planetDimensions = { x: PLANET_DIAMETER, y: PLANET_DIAMETER, z: PLANET_DIAMETER };
+    for (var i = 0; i < NUM_PLANETS; i++) {
+        var colorIndex = i % NUM_COLORS;
+
+        var orbitRadius = FIRST_ORBIT_OFFSET + i * deltaOrbit;
+        var phase = - i * deltaPhase;
+        var c = Math.cos(phase);
+        var s = Math.sin(phase);
+        var offset = scaleVec3(orbitRadius, { x: c, y: 0, z: s });
+        var position = addVec3s(SUN_POSITION, offset);
+        var totalMass = PLANET_MASS + SUN_MASS;
+        var speed = Math.sqrt(G * totalMass / orbitRadius);
+        var velocity = scaleVec3(speed, { x: s, y: 0, z: -c });
+        var gravity = scaleVec3(-G * totalMass / (orbitRadius * orbitRadius * orbitRadius), offset);
+
+        bodies.push(Entities.addEntity({
+            type: "Sphere",
+            name: "planet",
+            color: COLORS[colorIndex],
+            dimensions: planetDimensions,
+            position: position,
+            velocity: velocity,
+            gravity: ZERO_VEC3,
+            damping: 0.0,
+            angularDamping: 0.0,
+            dynamic: true,
+            restitution: 1.0,
+            density: PLANET_DENSITY,
+            gravity: gravity,
+            lifetime: LIFETIME
+        }));
+    }
+}
+createPlanets();
 
 Script.update.connect(function(dt) {
     // NOTE: due to script lag and naive integration this code will tend to pump energy into the system.
     // TOTRY: slightly increase damping to compensate
-    var pv = [];
-    var GmDt = G * planetMass * dt;
-    var GMDt = G * sunMass * dt;
-    for (var i = 0; i < NUM_PLANETS; i++) { 
-        pv.push(Entities.getEntityProperties(planets[i], ["position", "velocity"]));
+    var planetData = [];
+    var GmDt = G * (PLANET_MASS + PLANET_MASS);
+    var GMDt = G * (SUN_MASS + PLANET_MASS);
+    for (var i = 1; i < bodies.length; i++) {
+        var props = Entities.getEntityProperties(bodies[i], ["position", "velocity"]);
+        if (props.position !== undefined) {
+            props.bodyIndex = i;
+            props.gravity = ZERO_VEC3;
+            planetData.push(props);
+        }
     }
-    for (var i = 0; i < NUM_PLANETS; i++) {
-        for (var j = i + 1; j < NUM_PLANETS; j++) {
-            var offset = subtractVec3s(pv[j].position, pv[i].position);
-            var distance = lengthVec3(offset);
-            if (distance > 1.2 * PLANET_DIAMETER) {
-                // dV = a * dt = F/m * dt = (G * M / r^2) * dt
-                var dV = scaleVec3(GmDt / (distance * distance * distance), offset);
-                pv[i].velocity = addVec3s(pv[i].velocity, dV);
-                pv[j].velocity = subtractVec3s(pv[j].velocity, dV);
-            }
-        }
-        var offset = subtractVec3s(sunPosition, pv[i].position);
+    for (var i = 0; i < planetData.length; i++) {
+        // planet-sun interaction
+        var offset = subtractVec3s(SUN_POSITION, planetData[i].position);
         var distance = lengthVec3(offset);
-        if (distance > 1.2 * PLANET_DIAMETER) {
-            var dV = scaleVec3(GMDt / (distance * distance * distance), offset);
-            pv[i].velocity = addVec3s(pv[i].velocity, dV);
-            if (distance > SYSTEM_RADIUS) {
-                // check if planet is outward bound
-                var d = dotVec3s(offset, pv[i].velocity);
-                if (d < 0.0)  {
-                    // remove outward velocity to keep the planet in system
-                    pv[i].velocity = subtractVec3s(pv[i].velocity, scaleVec3(d / (distance * distance), offset));
-                }
+        planetData[i].gravity = addVec3s(planetData[i].gravity, scaleVec3(GMDt / (distance * distance * distance), offset));
+
+        // contain planets that want to escape the system
+        if (distance > SYSTEM_RADIUS) {
+            var vDotD = dotVec3s(offset, planetData[i].velocity);
+            if (vDotD < 0.0)  {
+                // remove outward velocity to keep the planet in system
+                var newVelocity = subtractVec3s(planetData[i].velocity, scaleVec3(vDotD / (distance * distance), offset));
+                Entities.editEntity(bodies[planetData[i].bodyIndex], { velocity: newVelocity });
             }
-            Entities.editEntity(planets[i], { velocity: pv[i].velocity });
         }
+
+        // planet-planet interaction
+        for (var j = i + 1; j < planetData.length; j++) {
+            offset = subtractVec3s(planetData[j].position, planetData[i].position);
+            distance = lengthVec3(offset);
+            var g = scaleVec3(GmDt / (distance * distance * distance), offset);
+            planetData[i].gravity = addVec3s(planetData[i].gravity, g);
+            planetData[j].gravity = subtractVec3s(planetData[j].gravity, g);
+        }
+
+        Entities.editEntity(bodies[planetData[i].bodyIndex], { gravity: planetData[i].gravity });
     }
 });
 
 Script.scriptEnding.connect(function scriptEnding() {
-    for (var i = 0; i < NUM_PLANETS; i++) {
-        Entities.deleteEntity(planets[i]);
+    for (var i = 0; i < bodies.length; i++) {
+        Entities.deleteEntity(bodies[i]);
     }
-    Entities.deleteEntity(sun);
+    bodies = [];
 });
